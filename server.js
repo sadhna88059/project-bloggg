8d070176f8f72c1974d7a88d4bef1b2ea76e427d
ramanand7 <69004414+ramanand7@users.noreply.github.com>
2022-02-12 23:22:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+
+
+app.get('/', async (req, res) => {
+  //
+  res.render('articles/home')
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
